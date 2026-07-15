5b866e2402d087d1138693df9732e90cb03a38b3
Harris-Lead <harris.leadbeater@frasers.group>
2026-07-15 17:20:19 +0100

    working on it buh

--- node-hard-parts/server.js
+++ node-hard-parts/server.js
@@ -1,24 +1,35 @@
 const http = require('http');
 const fs = require('fs');
+const { on } = require('cluster');
 
 function doOnRequest(request, response){
   // Send back a message saying "Welcome to Twitter"
   // code here...
-  response.end("yo")  
+  // response.end("Welcom to Twitter")
   if (request.method === 'GET' && request.url === '/') {
     // read the index.html file and send it back to the client
     // code here...
-    fs.createReadStream()
-
+    var page = fs.readFileSync("index.html", "utf8");
+    response.end(page);
+    
   }
   else if (request.method === 'POST' && request.url === '/sayHi') {
     // code here...
-    
+    fs.appendFileSync("hi_log.txt", "Somebody said hi.\n");
+    response.end('Someone said hi')
   }
   else if (request.method === 'POST' && request.url === '/greeting') {
     // accumulate the request body in a series of chunks
     // code here...
-    
+    let body = [];
+    request
+    .on('data', chuck =>{
+      body.push(chunk)
+    })
+    .on(end, () => {
+      body = Buffer.concat(body).toString();
+
+    })
   }
   else {
     // Handle 404 error: page not found
